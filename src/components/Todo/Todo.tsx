f5ce0a2b899cf8efbcdb9c33df7d1ecd2a5a59ac
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-21 13:21:47 -0300

    feat: add capability to delete and toggle done

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -1,19 +1,52 @@
+import { useContext } from 'react'
 import { Trash } from 'phosphor-react'
+
+import { Store } from '../../contexts/StoreCtx'
+import { ITodo } from '../../types'
 
 import styles from './Todo.module.css'
 
-export function Todo() {
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface TodoProps extends ITodo {}
+
+export function Todo({ id, description, isDone }: TodoProps) {
+  const { dispatch } = useContext(Store)
+
+  function handleDone() {
+    dispatch({
+      type: 'done',
+      payload: {
+        id,
+        description,
+        isDone
+      }
+    })
+  }
+
+  function handleDelete() {
+    dispatch({
+      type: 'delete',
+      payload: {
+        id,
+        description,
+        isDone
+      }
+    })
+  }
+
   return (
     <div className={styles.todo}>
       <label className={styles.container}>
-        <input type="checkbox" name="done" />
+        <input
+          type="checkbox"
+          name="done"
+          checked={isDone}
+          onChange={handleDone}
+        />
         <div className={styles.checkmark}></div>
       </label>
-      <span>
-        Integer urna interdum massa libero auctor neque turpis turpis semper.
-        Duis vel sed fames integer.
-      </span>
-      <button className={styles.todoDeleteBtn}>
+      <span>{description}</span>
+      <button className={styles.todoDeleteBtn} onClick={handleDelete}>
         <Trash size={24} />
       </button>
     </div>
