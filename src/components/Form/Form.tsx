3ed28f0b68e7063dfd729e25647ec22a7715bf16
Miqueias Sousa <miqueiassousa80@gmail.com>
2022-12-21 13:39:29 -0300

    feat: create a new todo

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,15 +1,45 @@
+import { ChangeEvent, FormEvent, useContext, useState } from 'react'
 import { PlusCircle } from 'phosphor-react'
+
+import { Store } from '../../contexts/StoreCtx'
 
 import styles from './Form.module.css'
 
+function uniqueId() {
+  return Math.round(Date.now() * Math.random()).toString()
+}
+
 export function Form() {
+  const [description, setDescription] = useState('')
+  const { dispatch } = useContext(Store)
+
+  function handleDescriptionChange(event: ChangeEvent<HTMLInputElement>) {
+    setDescription(event.target.value)
+  }
+
+  function handleCreate(event: FormEvent) {
+    event.preventDefault()
+
+    dispatch({
+      type: 'add',
+      payload: {
+        id: uniqueId(),
+        isDone: false,
+        description
+      }
+    })
+    setDescription('')
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleCreate}>
       <input
         className={styles.field}
         type="text"
         placeholder="Adicione uma nova tarefa"
         name="description"
+        value={description}
+        onChange={handleDescriptionChange}
       />
       <button className={styles.createButton} type="submit">
         <span>Criar</span>
